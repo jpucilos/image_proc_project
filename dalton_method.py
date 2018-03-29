6e0c31a90d2094ab0b45b79bae1b3610e4518de1
jpucilos <jpucilos@stevens.edu>
2018-03-29 11:48:28 -0400

    Alright so it turns out doing this was a lot simpler than I thought it would be

--- dalton_method.py
+++ dalton_method.py
@@ -1,4 +1,3 @@
-
 import PIL.Image
 import numpy as np
 import matplotlib as mpl
@@ -27,10 +26,11 @@ def sim_color_blindness(img, deficit = "d"):
 	  "p": np.array([[0, 2.02344, -2.52581], [0, 1, 0], [0, 0, 1]]),
 	  "t": np.array([[1, 0, 0], [0, 1, 0], [-0.395913, 0.801109, 0]]),
 	}
+	# rgb to lms colorspace conversion
 	rgb2lms = np.array([[17.8824, 43.5161, 4.11935],
 		               [3.45565, 27.1554, 3.86714],
 		               [0.0299566, 0.184309, 1.46709]])
-	# Precomputed inverse
+	# Precomputed inverse for lms to rgb
 	lms2rgb = np.array([[8.09444479e-02, -1.30504409e-01, 1.16721066e-01],
 		               [-1.02485335e-02, 5.40193266e-02, -1.13614708e-01],
 		               [-3.65296938e-04, -4.12161469e-03, 6.93511405e-01]])
@@ -42,11 +42,30 @@ def sim_color_blindness(img, deficit = "d"):
 	lms = trans_colorspace(rgb, rgb2lms)
 
 	simulated_lms = trans_colorspace(lms, cb_matrices[deficit])
-
 	return array_to_img(trans_colorspace(simulated_lms, lms2rgb))
+
+def daltonize(rgb, deficit='d'):
+	#Adjust color palette of an image to compensate color blindness.
+	sim_rgb = sim_color_blindness(rgb, deficit)
+	err2mod = np.array([[0, 0, 0], [0.7, 1, 0], [0.7, 0, 1]])
+	
+	# rgb - sim_rgb contains the color information that dichromats
+	# cannot see. err2mod rotates this to a part of the spectrum that
+	# they can see.
+	
+	rgb = rgb.convert('RGB')
+	rgb = np.asarray(rgb, dtype = float)
+	err = trans_colorspace(rgb - sim_rgb, err2mod)
+	dtpn = err + rgb
+	return array_to_img(dtpn)
+
 
 #Testing
 original_img = PIL.Image.open('test.jpg')
-original_img.show()   
-new_img = sim_color_blindness(original_img, "p")
+original_img.show()
+original_img_sim = sim_color_blindness(original_img, "p")
+original_img_sim.show()
+new_img = daltonize(original_img, "p")
 new_img.show()
+new_img_sim = sim_color_blindness(new_img, "p")
+new_img_sim.show()
